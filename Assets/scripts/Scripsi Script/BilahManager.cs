49f03c5b3372ad0a87c1d5d1c7f2e0cbab601912
kayubakar0 <suryaadrian970@gmail.com>
2022-09-18 14:45:37 +0700

    menyicil skripsi

--- Assets/scripts/Scripsi Script/BilahManager.cs
+++ Assets/scripts/Scripsi Script/BilahManager.cs
@@ -4,37 +4,64 @@ using UnityEngine;
 
 public class BilahManager : MonoBehaviour
 {
+    //Array
     public GameObject[] bilah;
     public GameObject[] tempatPukul;
-
-    private bool isWait = true;
+    public Material[] matColor;
     
+    //ListMusic
     private int[] notes = {2,3,2,3,1,2,3,2,3,5,6,5,4,2,1,6};
 
+    //Change color
+    Renderer rend;
 
     IEnumerator startDelay(){
-        isWait = true;//object now waits
-        yield return new WaitForSeconds(0.8f);
-        isWait = false; //object is no longer waiting
+        for(int j = 0; j < notes.Length; j++){
+            for(int i = 1; i < bilah.Length; i++){
+                Collider isBilah = bilah[i].GetComponent<Collider>();
+                Collider isPukul = tempatPukul[i].GetComponent<Collider>();
+                rend = bilah[i].GetComponent<Renderer>();
+                if(notes[j] == i+1){
+                    isBilah.enabled = false;
+                    isPukul.enabled = true;
+                    rend.material = matColor[1];
+                    yield return new WaitForSeconds(2f);
+                    isBilah.enabled = true;
+                    isPukul.enabled = false;
+                    rend.material = matColor[0];
+                }
+            }
+        }
+        // foreach (int note in notes)
+        // {
+        //     for(int i = 1; i < bilah.Length; i++){
+        //         Collider isBilah = bilah[i].GetComponent<Collider>();
+        //         Collider isPukul = tempatPukul[i].GetComponent<Collider>();
+        //         rend = bilah[i].GetComponent<Renderer>();
+        //         if(note == i+1){
+        //             isBilah.enabled = false;
+        //             isPukul.enabled = true;
+        //             rend.material = matColor[1];
+        //             yield return new WaitForSeconds(2f);
+        //             isBilah.enabled = true;
+        //             isPukul.enabled = false;
+        //             rend.material = matColor[0];
+        //         }
+        //     }
+        // }
     }
 
     void Update()
     {
-        Rythm();
-    }
-
-    private void Rythm(){
-        foreach (int note in notes)
-        {
-            for(int i = 1; i < bilah.Length + 1; i++){
-                if(note == i){
-                   bilah[i-1].GetComponent<Collider>().enabled = false;
-                   tempatPukul[i-1].GetComponent<Collider>().enabled = true;
-                   StartCoroutine(startDelay());
-                   bilah[i-1].GetComponent<Collider>().enabled = true;
-                   tempatPukul[i-1].GetComponent<Collider>().enabled = false;
-                }
-            }
-        }
-    }
+        StartCoroutine(startDelay());
+        // foreach (int note in notes){
+        //     for(int i = 1; i < bilah.Length + 1; i++){
+        //         if (note == i){
+        //             rend = bilah[i-1].GetComponent<Renderer>();
+        //             float lerp = Mathf.PingPong(Time.time, duration) / duration;
+        //             rend.material.Lerp(matColor[0], matColor[1], lerp);
+        //         }
+        //     }
+        // }
+    }       
 }
